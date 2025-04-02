35ca9587612a763745cc8a8e85c9edcad6bf5ff1
3A <74498981+3agithub@users.noreply.github.com>
2025-04-02 23:33:38 +0800

    Initial commit

---
+++ src/main/java/net/threeey/compoundedconcrete/CompoundedConcrete.java
@@ -0,0 +1,15 @@
+package net.threeey.compoundedconcrete;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CompoundedConcrete implements ModInitializer {
+	public static final String MOD_ID = "compoundedconcrete";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/threeey/compoundedconcrete/CompoundedConcreteClient.java
@@ -0,0 +1,10 @@
+package net.threeey.compoundedconcrete;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CompoundedConcreteClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
